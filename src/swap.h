c8d63437d0f2d6bef1c287b71b9e460da93c766c
yamad <jyamada1@gmail.com>
2011-12-09 08:20:46 -0500

    Add 32 and 64-bit byte swap macros

--- src/swap.h
+++ src/swap.h
@@ -6,8 +6,25 @@
 /* Return a byteswapped version of a 16-bit value */
 /* uint16_t swapBytes16(uint16_t); */
 
-#define _swap16(to_swap) ((uint16_t)(                   \
-    (((uint16_t)(to_swap) & (uint16_t)0x00ffU) << 8) |  \
-    (((uint16_t)(to_swap) & (uint16_t)0xff00U) >> 8)))
+#define _swap16(x) ((uint16_t)(                  \
+    (((uint16_t)(x) & (uint16_t)0x00ffU) << 8) | \
+    (((uint16_t)(x) & (uint16_t)0xff00U) >> 8)))
+
+#define _swap32(x) ((uint32_t)(                        \
+    (((uint32_t)(x) & (uint32_t)0x000000ffUL) << 24) | \
+    (((uint32_t)(x) & (uint32_t)0x0000ff00UL) <<  8) | \
+    (((uint32_t)(x) & (uint32_t)0x00ff0000UL) >>  8) | \
+    (((uint32_t)(x) & (uint32_t)0xff000000UL) >> 24)))
+
+#define _swap64(x) ((uint64_t)(                                 \
+    (((uint64_t)(x) & (uint64_t)0x00000000000000ffULL) << 56) | \
+    (((uint64_t)(x) & (uint64_t)0x000000000000ff00ULL) << 40) | \
+    (((uint64_t)(x) & (uint64_t)0x0000000000ff0000ULL) << 24) | \
+    (((uint64_t)(x) & (uint64_t)0x00000000ff000000ULL) <<  8) | \
+    (((uint64_t)(x) & (uint64_t)0x000000ff00000000ULL) >>  8) | \
+    (((uint64_t)(x) & (uint64_t)0x0000ff0000000000ULL) >> 24) | \
+    (((uint64_t)(x) & (uint64_t)0x00ff000000000000ULL) >> 40) | \
+    (((uint64_t)(x) & (uint64_t)0xff00000000000000ULL) >> 56)))
+    
 
 #endif /* LIBABF_SWAP_H_ */
--- test/testSwapBytes.c
+++ test/testSwapBytes.c
@@ -6,9 +6,22 @@
 void setUp(void) {}
 void tearDown(void) {}
 
-union twobytes {
+union twobytes
+{
     uint16_t as_int;
     char as_char[2];
+};
+
+union fourbytes 
+{
+    uint32_t as_int;
+    char as_char[4];
+};
+
+union eightbytes
+{
+    uint64_t as_int;
+    char as_char[8];
 };
 
 void test_swap16_should_swap_2bytes(void)
@@ -16,10 +29,54 @@ void test_swap16_should_swap_2bytes(void)
     union twobytes unswapped;
     unswapped.as_char[0] = 0xCA;
     unswapped.as_char[1] = 0xFE;
-    union twobytes swapped;
 
+    union twobytes swapped;
     swapped.as_int = _swap16(unswapped.as_int);
+    
     TEST_ASSERT_EQUAL_UINT(unswapped.as_char[0], swapped.as_char[1]);
     TEST_ASSERT_EQUAL_UINT(unswapped.as_char[1], swapped.as_char[0]);
 }
+
+void test_swap32_should_swap_4bytes(void)
+{
+    union fourbytes unswapped;
+    unswapped.as_char[0] = 0xBE;
+    unswapped.as_char[1] = 0xEF;
+    unswapped.as_char[2] = 0xCA;
+    unswapped.as_char[3] = 0xFE;
+
+    union fourbytes swapped;
+    swapped.as_int = _swap32(unswapped.as_int);
+
+    TEST_ASSERT_EQUAL_UINT(unswapped.as_char[0], swapped.as_char[3]);
+    TEST_ASSERT_EQUAL_UINT(unswapped.as_char[1], swapped.as_char[2]);
+    TEST_ASSERT_EQUAL_UINT(unswapped.as_char[2], swapped.as_char[1]);
+    TEST_ASSERT_EQUAL_UINT(unswapped.as_char[3], swapped.as_char[0]);    
+}
+
+void test_swap64_should_swap_8bytes(void)
+{
+    union eightbytes unswapped;
+    unswapped.as_char[0] = 0xBE;
+    unswapped.as_char[1] = 0xEF;
+    unswapped.as_char[2] = 0xCA;
+    unswapped.as_char[3] = 0xFE;
+    unswapped.as_char[4] = 0x12;
+    unswapped.as_char[5] = 0x34;
+    unswapped.as_char[6] = 0x56;
+    unswapped.as_char[7] = 0x78;
+
+    union eightbytes swapped;
+    swapped.as_int = _swap64(unswapped.as_int);
+
+    TEST_ASSERT_EQUAL_UINT(unswapped.as_char[0], swapped.as_char[7]);
+    TEST_ASSERT_EQUAL_UINT(unswapped.as_char[1], swapped.as_char[6]);
+    TEST_ASSERT_EQUAL_UINT(unswapped.as_char[2], swapped.as_char[5]);
+    TEST_ASSERT_EQUAL_UINT(unswapped.as_char[3], swapped.as_char[4]);    
+    TEST_ASSERT_EQUAL_UINT(unswapped.as_char[4], swapped.as_char[3]);    
+    TEST_ASSERT_EQUAL_UINT(unswapped.as_char[5], swapped.as_char[2]);    
+    TEST_ASSERT_EQUAL_UINT(unswapped.as_char[6], swapped.as_char[1]);    
+    TEST_ASSERT_EQUAL_UINT(unswapped.as_char[7], swapped.as_char[0]);    
+}
+
 

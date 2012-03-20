eba731031c51398ddc379e551ebda08776587872
yamad <jyamada1@gmail.com>
2012-03-20 13:11:42 -0400

    Correct read_*() to interpret buffer contents as native endian by default

--- src/deserialize.c
+++ src/deserialize.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <assert.h>
 #include <limits.h>
+#include <string.h>
 
 #include "deserialize.h"
 #include "swap.h"
@@ -17,56 +18,28 @@ uint8_t read_uint8(const char *buffer, size_t offset)
     return (uint8_t)read_char(buffer, offset);
 }
 
-uint16_t read_uint16(const char *buffer, size_t offset, bool swap) 
+uint16_t read_uint16(const char *buf, size_t offset, bool swap)
 {
-    uint8_t b1 = read_uint8(buffer, offset);
-    uint8_t b2 = read_uint8(buffer, offset+1);
-    uint16_t result = \
-        ((uint16_t)(                            \
-            ((uint16_t)b1 << 8) |               \
-            ((uint16_t)b2 << 0)));
+    uint16_t result;
+    memcpy(&result, buf, sizeof(uint16_t));
     if (swap)
         result = _swap16(result);
     return result;
 }
 
-uint32_t read_uint32(const char *buffer, size_t offset, bool swap)
+uint32_t read_uint32(const char *buf, size_t offset, bool swap)
 {
-    uint8_t b1 = read_uint8(buffer, offset);
-    uint8_t b2 = read_uint8(buffer, offset+1);
-    uint8_t b3 = read_uint8(buffer, offset+2);
-    uint8_t b4 = read_uint8(buffer, offset+3);
-    uint32_t result = \
-        ((uint32_t)(                            \
-            ((uint32_t)b1 << 24) |              \
-            ((uint32_t)b2 << 16) |              \
-            ((uint32_t)b3 <<  8) |              \
-            ((uint32_t)b4 <<  0)));
+    uint32_t result;
+    memcpy(&result, buf, sizeof(uint32_t));
     if (swap)
         result = _swap32(result);
     return result;
 }
 
-uint64_t read_uint64(const char *buffer, size_t offset, bool swap)
+uint64_t read_uint64(const char *buf, size_t offset, bool swap)
 {
-    uint8_t b1 = read_uint8(buffer, offset);
-    uint8_t b2 = read_uint8(buffer, offset+1);
-    uint8_t b3 = read_uint8(buffer, offset+2);
-    uint8_t b4 = read_uint8(buffer, offset+3);
-    uint8_t b5 = read_uint8(buffer, offset+4);
-    uint8_t b6 = read_uint8(buffer, offset+5);
-    uint8_t b7 = read_uint8(buffer, offset+6);
-    uint8_t b8 = read_uint8(buffer, offset+7);
-    uint64_t result = \
-        ((uint64_t)(                                                    \
-            ((uint64_t)b1 << 56) |                                      \
-            ((uint64_t)b2 << 48) |                                      \
-            ((uint64_t)b3 << 40) |                                      \
-            ((uint64_t)b4 << 32) |                                      \
-            ((uint64_t)b5 << 24) |                                      \
-            ((uint64_t)b6 << 16) |                                      \
-            ((uint64_t)b7 <<  8) |                                      \
-            ((uint64_t)b8 <<  0)));
+    uint64_t result;
+    memcpy(&result, buf, sizeof(uint64_t));
     if (swap)
         result = _swap64(result);
     return result;
@@ -94,7 +67,7 @@ int64_t read_int64(const char *buffer, size_t offset, bool swap)
 
 float read_float32(const char *buffer, size_t offset, bool swap)
 {
-    union 
+    union
     {
         float f;
         uint32_t i;

90b9f3a1b0f6160f16933b673342748a15ee7074
jasonyh <jasonyh@gmail.com>
2012-07-13 20:46:23 -0400

    Document and further test list interface

--- src/list.h
+++ src/list.h
@@ -3,13 +3,62 @@
 
 #include <stddef.h>
 
-/* Circular doubly-linked list implementation in standard C based on
- * Linux <linux/list.h> implementation (which is in GNU C) */
+/* Doubly-linked list implementation in standard C. Modified from
+ * Linux <linux/list.h> for general use.
+ *
+ * List nodes are stored inside of parent structures, which allows for
+ * a completely general implementation of lists. The data "payload" of
+ * each list entry is obtained by the `list_entry` macro which returns
+ * the containing struct of a list node. This design is copied from
+ * the design of linked lists in the Linux kernel.
+ *
+ *
+ * Note that the implementation is inverted with respect to the usual
+ * design of lists in which the data is stored within a list node. For
+ * instance, a common idiom is::
+ *
+ * struct foo {
+ *   int member_bar;
+ *   int member_baz;
+ *   struct foo *next;
+ *   struct foo *prev;
+ * };
+ *
+ * or for more generality,
+ *
+ * struct list_node {
+ *   void *data;
+ *   struct list_node *next;
+ *   struct list_node *prev;
+ * };
+ *
+ * This implementation does neither of the above and instead opts for
+ * storing the list inside the data::
+ *
+ * struct foo {
+ *    int member_bar;
+ *    int member_baz;
+ *    struct list_head list;
+ * };
+ *
+ * In particular, this implementation (i) generalizes list handling,
+ * (ii) eases structure allocation, and (iii) cleanly handles
+ * including a structure in multiple lists.
+ *
+ * Changes from linux/list.h:
+ *
+ *   * remove "inline" declaration specifiers for C89 compatibility
+ *   * change implementation of `container_of` to use only ANSI C
+ *   * add `type` member to macros that depended on `typeof` (typeof is
+ *     a GCC extension)
+ *   * only functions that are used are implemented
+ */
 
 /**
  * list entry type
  *
- * To use, add a member of this type to the desired structure. for instance,
+ * Add a member of this type to make a linked-list out of a
+ * struct. For instance,
  *
  * struct foo {
  *   int bar;
@@ -35,10 +84,127 @@ struct list_head {
 /**
  * INIT_LIST_HEAD - initialize list head at run-time
  */
-static inline void INIT_LIST_HEAD(struct list_head *list)
+static void INIT_LIST_HEAD(struct list_head *list)
 {
     list->next = list;
     list->prev = list;
+}
+
+/**
+ * insert a new entry between two known consecutive entries.
+ *
+ * only use if the next/prev entries are already known
+ */
+static void __list_add(struct list_head *new,
+                       struct list_head *prev,
+                       struct list_head *next)
+{
+    next->prev = new;
+    new->next = next;
+    new->prev = prev;
+    prev->next = new;
+}
+
+/**
+ * remove entry from list by making prev/next entries point to each
+ * other.
+ */
+static void __list_del(struct list_head *prev, struct list_head *next)
+{
+    next->prev = prev;
+    prev->next = next;
+}
+
+/**
+ * remove entry from list. entry is in undefined state.
+ */
+static void __list_del_entry(struct list_head *entry)
+{
+    __list_del(entry->prev, entry->next);
+    entry->next = NULL;
+    entry->prev = NULL;
+}
+
+/**
+ * list_add - add a new entry after specified head
+ * @new:  entry to add
+ * @head: list entry to insert after
+ */
+static void list_add(struct list_head *new, struct list_head *head)
+{
+    __list_add(new, head, head->next);
+}
+
+/**
+ * list_add_tail - add a new entry before specified head
+ * @new:  entry to add
+ * @head: list entry to insert before
+ */
+static void list_add_tail(struct list_head *new, struct list_head *head)
+{
+    __list_add(new, head->prev, head);
+}
+
+/**
+ * list_replace - replace old entry by new one
+ * @old : the element to be replaced
+ * @new : the new element to insert
+ *
+ * If @old was empty, it will be overwritten.
+ */
+static void list_replace(struct list_head *old,
+                         struct list_head *new)
+{
+    new->next = old->next;
+    new->next->prev = new;
+    new->prev = old->prev;
+    new->prev->next = new;
+}
+
+static void list_replace_init(struct list_head *old,
+                              struct list_head *new)
+{
+    list_replace(old, new);
+    INIT_LIST_HEAD(old);
+}
+
+/**
+ * list_del_init - deletes entry from list and reinitialize it.
+ * @entry: the element to delete from the list.
+ */
+static void list_del_init(struct list_head *entry)
+{
+    __list_del_entry(entry);
+    INIT_LIST_HEAD(entry);
+}
+
+/**
+ * list_is_last - tests whether @list is the last entry in list @head
+ * @list: the entry to test
+ * @head: the head of the list
+ */
+static int list_is_last(const struct list_head *list,
+                        const struct list_head *head)
+{
+    return list->next == head;
+}
+
+/**
+ * list_empty - tests whether a list is empty
+ * @head: the list to test.
+ */
+static int list_empty(const struct list_head *head)
+{
+    return head->next == head;
+}
+
+/**
+ * list_is_singular - tests whether a list has just one entry.
+ * @head: the list to test.
+ */
+static int list_is_singular(const struct list_head *head)
+{
+    return !list_empty(head) && (head->next == head->prev);
 }
 
 /**
@@ -61,5 +227,54 @@ static inline void INIT_LIST_HEAD(struct list_head *list)
  */
 #define list_entry(ptr, type, member)           \
     container_of(ptr, type, member)
+
+/**
+ * list_for_each - iterate over a list
+ * @pos:  the struct list_head to use as a loop counter
+ * @head: the head of the list
+ */
+#define list_for_each(pos, head)                                \
+    for (pos = (head)->next; pos != (head); pos = pos->next)
+
+/**
+ * list_for_each_prev -	iterate over a list backwards
+ * @pos:	the &struct list_head to use as a loop cursor.
+ * @head:	the head for your list.
+ */
+#define list_for_each_prev(pos, head)                           \
+    for (pos = (head)->prev; pos != (head); pos = pos->prev)
+
+/**
+ * list_for_each_safe - iterate over a list safe against removal of list entry
+ * @pos:	the &struct list_head to use as a loop cursor.
+ * @n:		another &struct list_head to use as temporary storage
+ * @head:	the head for your list.
+ */
+#define list_for_each_safe(pos, n, head)                        \
+    for (pos = (head)->next, n = pos->next; pos != (head);      \
+         pos = n, n = pos->next)
+
+/**
+ * list_for_each_prev_safe - iterate over a list backwards safe against removal of list entry
+ * @pos:	the &struct list_head to use as a loop cursor.
+ * @n:		another &struct list_head to use as temporary storage
+ * @head:	the head for your list.
+ */
+#define list_for_each_prev_safe(pos, n, head)   \
+    for (pos = (head)->prev, n = pos->prev;     \
+         pos != (head);                         \
+         pos = n, n = pos->prev)
+
+/**
+ * list_for_each_entry - iterate over list of given type
+ * @pos:	the type * to use as a loop cursor.
+ * @type:       the type of struct that @pos is
+ * @head:	the head for your list.
+ * @member:	the name of the list_struct within the struct.
+ */
+#define list_for_each_entry(pos, type, head, member)                    \
+    for (pos = list_entry((head)->next, type, member);                  \
+         &pos->member != (head);                                        \
+         pos = list_entry(pos->member.next, type, member))
 
 #endif /* _LLIST_H_ */
--- test/test_list.c
+++ test/test_list.c
@@ -13,6 +13,13 @@ struct fox {
     struct list_head list;
 };
 
+struct hare {
+    unsigned long size;
+    unsigned long weight;
+    bool is_fast;
+    struct list_head list;
+};
+
 void setUp(void) {}
 void tearDown(void) {}
 
@@ -42,4 +49,76 @@ void test_list_entry(void)
         .list = LIST_HEAD_INIT(f.list)
     };
     TEST_ASSERT_EQUAL_PTR(&f, list_entry(&f.list, struct fox, list));
+}
+
+void test_LIST_HEAD_creates_and_inits_list_var(void)
+{
+    LIST_HEAD(test_list);
+    TEST_ASSERT_EQUAL_PTR(&test_list, test_list.next);
+    TEST_ASSERT_EQUAL_PTR(&test_list, test_list.prev);
+}
+
+void test_list_add_single_entry(void)
+{
+    LIST_HEAD(test_list);
+    struct fox foxA = {
+        .is_fantastic = true,
+        .list = LIST_HEAD_INIT(foxA.list)
+    };
+    list_add(&foxA.list, &test_list);
+    TEST_ASSERT_EQUAL_PTR(&foxA.list, test_list.next);
+    TEST_ASSERT_EQUAL_PTR(&foxA.list, test_list.prev);
+}
+
+void test_list_add_multiple_entries(void)
+{
+    LIST_HEAD(test_list);
+    struct fox foxA = {
+        .is_fantastic = true,
+        .list = LIST_HEAD_INIT(foxA.list)
+    };
+    struct fox foxB = {
+        .is_fantastic = true,
+        .list = LIST_HEAD_INIT(foxB.list)
+    };
+
+    /* create links: test_list -> foxA -> foxB */
+    list_add(&foxB.list, &test_list);
+    list_add(&foxA.list, &test_list);
+
+    /* test forward pointers */
+    TEST_ASSERT_EQUAL_PTR(&foxA.list, test_list.next);
+    TEST_ASSERT_EQUAL_PTR(&foxB.list, foxA.list.next);
+    TEST_ASSERT_EQUAL_PTR(&test_list, foxB.list.next);
+
+    /* test backward pointers */
+    TEST_ASSERT_EQUAL_PTR(&foxB.list, test_list.prev);
+    TEST_ASSERT_EQUAL_PTR(&foxA.list, foxB.list.prev);
+    TEST_ASSERT_EQUAL_PTR(&test_list, foxA.list.prev);
+}
+
+void test_struct_with_list_member(void)
+{
+    struct list_of_lists {
+        struct list_head fox_list;
+        struct list_head hare_list;
+    };
+
+    struct list_of_lists lol = {
+        .fox_list = LIST_HEAD_INIT(lol.fox_list),
+        .hare_list = LIST_HEAD_INIT(lol.hare_list)
+    };
+
+    struct fox a_fox = {
+        .is_fantastic = true,
+        .list = LIST_HEAD_INIT(a_fox.list)
+    };
+
+    struct hare a_hare = {
+        .is_fast = false,
+        .list = LIST_HEAD_INIT(a_hare.list)
+    };
+
+    list_add(&a_fox.list, &lol.fox_list);
+    list_add(&a_hare.list, &lol.hare_list);
 }
